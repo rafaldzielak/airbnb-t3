2db4dc6e0e2ff2322d1cdbb22e002d87bd1c16d4
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-04-23 10:43:42 +0200

    Tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Nunito } from "next/font/google";
 
 export const metadata = {
   title: "Airbnb",
   description: "Airbnb clone",
 };
 
+const font = Nunito({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hi</div>;
+  return <div className='text-rose-500 text-2xl'>Hi</div>;
 }
