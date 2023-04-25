53c34119231346dbc13fc511723e4c349a9847f6
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-04-25 19:42:03 +0200

    Login with google/github UI

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Modal from "./Modal";
 import Heading from "../Heading/Heading";
 import Input from "../Inputs/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button/Button";
 
 const RegisterModal = () => {
   const { isOpen, onClose, onOpen } = useRegisterModal();
@@ -39,6 +40,22 @@ const RegisterModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button outline label='Continue with Google' icon={FcGoogle} onClick={() => {}} />
+      <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => {}} />
+      <div className='text-neutral-500 text-center mt-4 font-light'>
+        <div className='flex flex-row justify-center Zgap-2'>
+          <div>Alread have an account?</div>
+          <div onClick={onClose} className='text-neutral-800 cursor-pointer hover:underline'>
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -47,7 +64,9 @@ const RegisterModal = () => {
       actionLabel='Continue'
       onClose={onClose}
       onSubmit={handleSubmit(onSubmit)}
-      body={bodyContent}></Modal>
+      body={bodyContent}
+      footer={footerContent}
+    />
   );
 };
 
