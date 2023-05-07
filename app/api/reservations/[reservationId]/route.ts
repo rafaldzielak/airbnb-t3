63242205b0f3b4981b4d170884f7dfd84664f467
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-05-07 11:56:36 +0200

    Delete reservation API route

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,24 @@
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface Params {
+  reservationId?: string;
+}
+
+export async function DELETE(request: Request, { params }: { params: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return NextResponse.error();
+
+  const { reservationId } = params;
+  if (!reservationId || typeof reservationId !== "string") throw new Error("Invalid ID");
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { Listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
