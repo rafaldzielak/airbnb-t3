19ec9a79fa24c6b6a7cb79b750cc5a0c0adc8405
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-05-08 20:50:05 +0200

    Get users properties action

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,11 +1,19 @@
 import prisma from "@/app/libs/prismadb";
 import { SafeListing } from "../types";
 
+export interface ListingsParams {
+  userId?: string;
+}
+
 // This allows to get use the user without API call via server component
 
-export default async function getListings() {
+export default async function getListings(params: ListingsParams) {
   try {
-    const listings = await prisma.listing.findMany({ orderBy: { createdAt: "desc" } });
+    const { userId } = params;
+    let query: any = {};
+    if (userId) query.userId = userId;
+
+    const listings = await prisma.listing.findMany({ orderBy: { createdAt: "desc" }, where: query });
     const safeListings: SafeListing[] = listings.map((listing) => ({ ...listing, createdAt: listing.createdAt.toISOString() }));
     return safeListings;
   } catch (error: any) {
